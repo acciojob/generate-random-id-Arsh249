42c569f060b8ae9baa180da083f2668e1a7dec71
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-04-07 14:31:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function makeid(l) {
-  // write your code here
+function makeid(length) {
+   let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter ++;
+    }
+    return result; 
 }
-
+ 
 // Do not change the code below.
 const l = prompt("Enter a number.");
 alert(makeid(l));
